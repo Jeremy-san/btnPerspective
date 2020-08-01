87fae9a1d62d99bab0c5688c71ce298e8a101ae6
Jeremy-san <shehata.jeremy@gmail.com>
2020-08-01 02:29:24 +0200

    final test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BtnComponent } from './btn/btn.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BtnComponent
   ],
   imports: [
     BrowserModule,
